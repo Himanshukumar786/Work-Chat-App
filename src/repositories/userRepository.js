02acefcde32e6d2b26fe6391690274e3682b4c05
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-11-13 02:04:46 +0530

    Added repository signature

---
+++ src/repositories/userRepository.js
@@ -0,0 +1,13 @@
+export const getUserByName = (email) => {};
+
+export const getUserByEmail = (name) => {};
+
+export const createUser = (user) => {};
+
+export const getUsers = () => {};
+
+export const getUserById = (id) => {};
+
+export const deleteUser = (id) => {};
+
+export const updateUser = (id, user) => {};
