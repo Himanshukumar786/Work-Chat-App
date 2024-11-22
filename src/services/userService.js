4814bbeae65ce7c476f8fc2c58912ac0312ee122
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-11-22 23:13:48 +0530

    Added signin with zod validations

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,9 @@
+import bcrypt from 'bcrypt';
+import { StatusCodes } from 'http-status-codes';
+
 import userRepository from '../repositories/userRepository.js';
+import { createJWT } from '../utils/common/authUtils.js';
+import ClientError from '../utils/errors/clientError.js';
 import ValidationError from '../utils/errors/validationError.js';
 
 export const signUpService = async (data) => {
@@ -13,7 +18,7 @@ export const signUpService = async (data) => {
 
             throw new ValidationError(
                 {
-                error: error.errors
+                    error: error.errors
                 },
                 error.message
             );
@@ -22,10 +27,43 @@ export const signUpService = async (data) => {
             
             throw new ValidationError(
                 {
-                error: ['A user with same email or username already exists']
+                    error: ['A user with same email or username already exists']
                 },
                 'A user with same email or username already exists'
             );
         }
+    }
+};
+
+export const signInService = async (data) => {
+    try {
+      const user = await userRepository.getByEmail(data.email);
+
+      if (!user) {
+        throw new ClientError({
+            explanation: 'Invalid data sent from the client',
+            message: 'No registered user found with this email',
+            statusCode: StatusCodes.NOT_FOUND
+        });
+      }
+      
+      // match the incoming password with the hashed password
+      const isMatch = bcrypt.compareSync(data.password, user.password);
+      if (!isMatch) {
+        throw new ClientError({
+            explanation: 'Invalid data sent from the client',
+            message: 'Invalid password, please try again',
+            statusCode: StatusCodes.BAD_REQUEST
+        });
+      }
+      return {
+        username: user.username,
+        avatar: user.avatar,
+        email: user.email,
+        token: createJWT({ id: user._id, email: user.email })
+      };
+    } catch (error) {
+        console.log('User service error', error);
+        throw error;
     }
 };
