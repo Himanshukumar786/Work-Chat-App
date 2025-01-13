22ce7c134146e0a83b3618ce7495a07cc319795b
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2025-01-13 22:28:42 +0530

    Added simple bug fix

--- src/services/workspaceService.js
+++ src/services/workspaceService.js
@@ -226,11 +226,11 @@ export const getWorkspaceByJoinCodeService = async (joinCode, userId) => {
         });
       }
       const isMember = isUserMemberOfWorkspace(workspace, memberId);
-      if (!isMember) {
+      if (isMember) {
         throw new ClientError({
-          explanation: 'User is not a member of the workspace',
-          message: 'User is not a member of the workspace',
-          statusCode: StatusCodes.UNAUTHORIZED
+            explanation: 'User is already a member of the workspace',
+            message: 'User is already a member of the workspace',
+            statusCode: StatusCodes.UNAUTHORIZED
         });
       }
       const response = await workspaceRepository.addMemberToWorkspace(
