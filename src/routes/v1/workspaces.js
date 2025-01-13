6befa7291f42b210d3f833fb91d0ae4e940ad4d3
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2025-01-13 19:54:12 +0530

    Added delete workspace api

--- src/routes/v1/workspaces.js
+++ src/routes/v1/workspaces.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { createWorkspaceController, getWorkspacesUserIsMemberOfController } from '../../controllers/workspaceController.js';
+import { createWorkspaceController, deleteWorkspaceController, getWorkspacesUserIsMemberOfController } from '../../controllers/workspaceController.js';
 import { isAuthenticated } from '../../middlewares/authMiddleware.js';
 import { createWorkspaceSchema } from '../../validators/workspaceSchema.js';
 import { validate } from '../../validators/zodValidator.js';
@@ -15,5 +15,6 @@ router.post(
 );
 
 router.get('/', isAuthenticated, getWorkspacesUserIsMemberOfController);
+router.delete('/:workspaceId', isAuthenticated, deleteWorkspaceController);
 
 export default router;
